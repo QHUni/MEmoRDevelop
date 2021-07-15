f5bd4f847b9da4c27864807887e7519a7d2adeeb
QHUni <halo.qhy@gmail.com>
2021-07-15 17:09:16 +0800

    Inital version on 2021.7.15

---
+++ MEmoR/model/model.py
@@ -0,0 +1,195 @@
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+from base import BaseModel
+from model.TfEncoderWithMemory import BertEncoderWithMemory
+
+class ScaledDotProductAttention(nn.Module):
+    """ Scaled Dot-Product Attention """
+
+    def __init__(self, temperature, attn_dropout=0.1):
+        super().__init__()
+        self.temperature = temperature
+        self.dropout = nn.Dropout(attn_dropout)
+
+    def forward(self, q, k, v, mask=None):
+
+        attn = torch.matmul(q / self.temperature, k.transpose(0, 1))
+
+        if mask is not None:
+            attn = attn.masked_fill(mask == 0, -1e9)
+
+        attn = self.dropout(F.softmax(attn, dim=-1))
+        output = torch.matmul(attn, v)
+
+        return output, attn
+
+
+class AMER(BaseModel):
+    def __init__(self, config):
+        super().__init__()
+
+    def initialize(self, config, device):
+
+        n_classes = 9 if config['emo_type'] == 'primary' else 14
+        D_e = config["model"]["args"]["D_e"]
+        D_v = config["visual"]["dim_env"] + config["visual"]["dim_face"] + config["visual"]["dim_obj"]
+        D_a = config["audio"]["feature_dim"]
+        D_t = config["text"]["feature_dim"]
+        D_p = config["personality"]["feature_dim"]
+        
+        self.attn = ScaledDotProductAttention((4 * D_e) ** 0.5, attn_dropout=0)
+
+        #qhy add
+        #注意这里的config格式不一样
+        self.encoder = BertEncoderWithMemory(config)
+
+        self.enc_v = nn.Sequential(
+            nn.Linear(D_v, D_e * 4),
+            nn.ReLU(),
+            nn.Linear(D_e * 4, D_e * 3),
+            nn.ReLU(),
+            nn.Linear(D_e * 3, 2 * D_e),
+        )
+
+        self.enc_a = nn.Sequential(
+            nn.Linear(D_a, D_e * 8),
+            nn.ReLU(),
+            nn.Linear(D_e * 8, D_e * 4),
+            nn.ReLU(),
+            nn.Linear(D_e * 4, 2 * D_e),
+        )
+        self.enc_t = nn.Sequential(
+            nn.Linear(D_t, D_e * 4),
+            nn.ReLU(),
+            nn.Linear(D_e * 4, 2 * D_e),
+        )
+
+        self.enc_p = nn.Sequential(
+            nn.Linear(D_p, D_e * 4),
+            nn.ReLU(),
+            nn.Linear(D_e * 4, 2 * D_e),
+        )
+
+        self.out_layer = nn.Sequential(
+            nn.Linear(4 * D_e, 2 * D_e), 
+            nn.ReLU(), 
+            nn.Linear(2 * D_e, n_classes)
+        )
+
+        unified_d = 14 * D_e
+
+        self.fusion_layer = nn.Linear(unified_d, 4 * D_e)
+
+    def forward(self, U_v, U_a, U_t, U_p, M_v, M_a, M_t, seq_lengths, target_loc, seg_len, n_c):
+        # Encoders
+        V_e, A_e, T_e, P_e = self.enc_v(U_v), self.enc_a(U_a), self.enc_t(U_t), self.enc_p(U_p)
+
+        U_all = []
+
+        for i in range(M_v.shape[0]):
+            target_moment, target_character = -1, -1
+            for j in range(target_loc.shape[1]):
+                if target_loc[i][j] == 1:
+                    target_moment = j % int(seg_len[i].cpu().numpy())
+                    target_character = int(j / seg_len[i].cpu().numpy())
+                    break
+            
+            # 第1部分原始的特征变换
+            inp_V = V_e[i, : seq_lengths[i], :].reshape((n_c[i], seg_len[i], -1)).transpose(0, 1)
+            inp_T = T_e[i, : seq_lengths[i], :].reshape((n_c[i], seg_len[i], -1)).transpose(0, 1)
+            inp_A = A_e[i, : seq_lengths[i], :].reshape((n_c[i], seg_len[i], -1)).transpose(0, 1)
+            inp_P = P_e[i, : seq_lengths[i], :].reshape((n_c[i], seg_len[i], -1)).transpose(0, 1)
+
+            # 随机mask掉一部分特征做推理
+            mask_V = M_v[i, : seq_lengths[i]].reshape((n_c[i], seg_len[i])).transpose(0, 1)
+            mask_T = M_t[i, : seq_lengths[i]].reshape((n_c[i], seg_len[i])).transpose(0, 1)
+            mask_A = M_a[i, : seq_lengths[i]].reshape((n_c[i], seg_len[i])).transpose(0, 1)
+
+
+            # qhy add from MART:encoder
+            # 参数1：prev_ms
+            prev_ms = [None] * self.config.num_hidden_layers
+            # 2:embeddings
+            self.embeddings = BertEmbeddingsWithVideo(config, add_postion_embeddings=True)
+            embeddings = self.embeddings(input_ids, video_features, token_type_ids)  # (N, L, D)
+            # 3:input masks
+            input_masks_list = [e["input_mask"] for e in batched_data]  # input_masks_list: [(N, L)] * step_size with 1 indicates valid bits
+            
+            prev_ms, encoded_layer_outputs = self.encoder(
+            prev_ms, embeddings, input_masks, output_all_encoded_layers=False)  # both outputs are list
+
+            '''
+            问题
+            1.prev_ms是什么？
+            2.N,L,D等参数的含义？可能和tf的代码有关？
+            3.encoder前的concat操作在哪里？
+            4.transformerXL?对比模型
+
+
+            note：
+            注意这是python2.7
+
+            核心：
+            1.三个输入和输出格式的适配
+            2.config的适配
+            '''
+            
+            
+
+
+            # Concat with personality embedding
+            inp_V = torch.cat([inp_V, inp_P], dim=2)
+            inp_A = torch.cat([inp_A, inp_P], dim=2)
+            inp_T = torch.cat([inp_T, inp_P], dim=2)
+
+            U = []
+
+            
+            # 第2部分原始的cross-attention：两个循环
+            for k in range(n_c[i]):
+                new_inp_A, new_inp_T, new_inp_V = inp_A.clone(), inp_T.clone(), inp_V.clone(),
+                
+                # Modality-level inter-personal attention
+                for j in range(seg_len[i]):
+                    att_V, _ = self.attn(inp_V[j, :], inp_V[j, :], inp_V[j, :], mask_V[j, :])
+                    att_T, _ = self.attn(inp_T[j, :], inp_T[j, :], inp_T[j, :], mask_T[j, :])
+                    att_A, _ = self.attn(inp_A[j, :], inp_A[j, :], inp_A[j, :], mask_A[j, :])
+                    new_inp_V[j, :] = att_V + inp_V[j, :]
+                    new_inp_A[j, :] = att_A + inp_A[j, :]
+                    new_inp_T[j, :] = att_T + inp_T[j, :]
+
+                # Modality-level intra-personal attention
+                att_V, _ = self.attn(new_inp_V[:, k], new_inp_V[:, k], new_inp_V[:, k], mask_V[:, k])
+                att_A, _ = self.attn(new_inp_A[:, k], new_inp_A[:, k], new_inp_A[:, k], mask_A[:, k])
+                att_T, _ = self.attn(new_inp_T[:, k], new_inp_T[:, k], new_inp_T[:, k], mask_T[:, k])
+
+                # Residual connection
+                inner_V = (att_V[target_moment] + new_inp_V[target_moment][k]).squeeze()
+                inner_A = (att_A[target_moment] + new_inp_A[target_moment][k]).squeeze()
+                inner_T = (att_T[target_moment] + new_inp_T[target_moment][k]).squeeze()
+
+                # 第3部分特征融合
+                # Multimodal fusion
+                inner_U = self.fusion_layer(torch.cat([inner_V, inner_A, inner_T, inp_P[0][k]]))
+
+                U.append(inner_U)
+
+            # 第3部分之后的又一次attention处理，需要删掉，注意输出格式
+            if len(U) == 1:
+                # Only one character in this sample
+                U_all.append(U[0])
+            else:
+                # Person-level Inter-personal Attention
+                U = torch.stack(U, dim=0)
+                output, _ = self.attn(U, U, U)
+                U = U + output
+                U_all.append(U[target_character])
+
+        U_all = torch.stack(U_all, dim=0)
+        # Classification
+        log_prob = self.out_layer(U_all)
+        log_prob = F.log_softmax(log_prob)
+
+        return log_prob
+
